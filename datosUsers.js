903df90a44e9d5685c7c1c0d4b4370f1724f48e4
IronEva2312 <agusmaty23@gmail.com>
2024-10-08 21:38:29 -0500

    Refactor

--- datosUsers.js
+++ datosUsers.js
@@ -17,7 +17,7 @@ obtenerUsaurios();
 const pintarUsuarios = (data) => 
 {
     let body = ""
-    for(let i = 0 ; i < data.length; i++){
+    for(let i = data.length - 1 ; i >= 0; i--){
         body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`;
     }
     document.getElementById("data").innerHTML=body;
